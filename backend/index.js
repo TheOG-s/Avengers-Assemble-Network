ff9e550376c3f8408c756ad85fd3509da7f992b8
srvmandal24 <srvmandal24@gmail.com>
2024-10-30 20:26:14 +0530

    Connected mongoDB and cloudinary

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectdb from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
+
+// App config
+const app = express();
+const port = process.env.PORT || 8000;
+connectdb();
+connectCloudinary();
+
+// Middlewares
+app.use(express.json())
+app.use(cors())
+
+
+app.get("/", (req, res) => {
+    res.send("Backend Running.");
+})
+
+app.listen(port, () => {
+    console.log("Listening to port : " + port)
+})
