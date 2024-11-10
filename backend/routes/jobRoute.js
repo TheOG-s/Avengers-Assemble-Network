4db31361f73e16559663db6ddf7189b91625ef17
srvmandal24 <srvmandal24@gmail.com>
2024-11-10 23:30:20 +0530

    Added routes and controllers to view and add jobs by company.

--- backend/routes/jobRoute.js
+++ backend/routes/jobRoute.js
@@ -4,13 +4,22 @@ import {
   showJob,
   showAllJob,
   removeJob,
+  showCompanyJob,
+  showAllActiveJob,
+  showCompanyActiveJob,
+  changeStatus,
 } from "../controllers/jobController.js";
+import protectCompanyRoute from "../middlewares/authCompany.js";
 
 const jobRouter = express.Router();
 
-jobRouter.post("/add", addJob);
-jobRouter.get("/show", showJob);
-jobRouter.get("/showAll", showAllJob);
-jobRouter.post("/remove", removeJob);
+jobRouter.post("/add", protectCompanyRoute, addJob);
+jobRouter.get("/show/:jobId", showJob);
+jobRouter.get("/showall", showAllJob);
+jobRouter.get("/showactive", showAllActiveJob);
+jobRouter.get("/showall/:companyId", showCompanyJob);
+jobRouter.get("/showactive/:companyId", showCompanyActiveJob);
+jobRouter.post("/changestatus/:jobId", protectCompanyRoute, changeStatus);
+jobRouter.post("/remove/:jobId", protectCompanyRoute, removeJob);
 
 export default jobRouter;
